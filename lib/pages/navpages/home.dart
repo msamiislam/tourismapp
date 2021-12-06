b3dc04e917a035bc3b074f7202a61febceac9b10
sami-islam99 <sami.islam786@gmail.com>
2021-12-06 22:24:10 +0500

    minor update home ui, add search page

--- lib/pages/navpages/home.dart
+++ lib/pages/navpages/home.dart
@@ -1,8 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get/get.dart';
-import 'package:tourismapp/pages/login.dart';
 import 'package:tourismapp/utils/colors.dart';
 import 'package:tourismapp/widgets/large_txt.dart';
 import 'package:tourismapp/widgets/simple_txt.dart';
@@ -42,6 +38,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     TabController _tabController = TabController(length: _tabs.length, vsync: this);
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         iconTheme: IconThemeData(color: Colors.black),
         elevation: 0.0,
@@ -50,21 +47,21 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           SizedBox(width: 10.0),
         ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(accountName: Text("accountName"), accountEmail: Text("accountEmail")),
-            ListTile(
-              leading: Icon(FontAwesomeIcons.signOutAlt),
-              title: AppText( "Logout"),
-              onTap: () {
-                FirebaseAuth.instance.signOut();
-                Get.offAll(() => LoginPage());
-              },
-            ),
-          ],
-        ),
-      ),
+      // drawer: Drawer(
+      //   child: ListView(
+      //     children: [
+      //       UserAccountsDrawerHeader(accountName: Text("accountName"), accountEmail: Text("accountEmail")),
+      //       ListTile(
+      //         leading: Icon(FontAwesomeIcons.signOutAlt),
+      //         title: AppText( "Logout"),
+      //         onTap: () {
+      //           FirebaseAuth.instance.signOut();
+      //           Get.offAll(() => LoginPage());
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
       body: ListView(
         children: [
           //upper Menu bar icon + image
