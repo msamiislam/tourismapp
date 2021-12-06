533e6d86ef69b6b4eb7819218372c22344bfcfd2
Muhammad Sami Islam <69349164+Sami-Islam99@users.noreply.github.com>
2021-12-06 20:08:51 +0500

    Theme (#3)
    * app colors updated
    
    * fonts added
    
    * colors scheme added
    
    Co-authored-by: sami-islam99 <sami.islam786@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:tourismapp/pages/splash.dart';
+import 'package:tourismapp/utils/colors.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +23,26 @@ class MyApp extends StatelessWidget {
       title: 'Adventuree',
       localizationsDelegates: [FormBuilderLocalizations.delegate],
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        scaffoldBackgroundColor: AppColors.scaffoldBg,
+        primarySwatch: AppColors.primarySwatch,
+        colorScheme: ColorScheme(
+          primary: AppColors.primary,
+          onPrimary: AppColors.onPrimary,
+          primaryVariant: AppColors.primaryVariant,
+          secondary: AppColors.secondary,
+          onSecondary: AppColors.onSecondary,
+          secondaryVariant: AppColors.secondaryVariant,
+          background: AppColors.background,
+          onBackground: AppColors.onBackground,
+          surface: AppColors.surface,
+          onSurface: AppColors.onSurface,
+          error: Theme.of(context).errorColor,
+          onError: AppColors.white,
+          brightness: Brightness.light,
+        )
+      ),
       home: SplashPage(),
     );
   }
