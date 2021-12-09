419c4fb903c5e9b1a5b4d8373d2a1871405da11f
sami-islam99 <sami.islam786@gmail.com>
2021-12-10 01:16:31 +0500

    add guide booking page

--- lib/pages/guide/dashboard.dart
+++ lib/pages/guide/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tourismapp/pages/guide/navpages/notifications.dart';
 
 import 'navpages/home.dart';
 
@@ -12,7 +13,7 @@ class GuideDashbboardPage extends StatefulWidget {
 class _GuideDashbboardPageState extends State<GuideDashbboardPage> {
   List pages = [
     GuideHomePage(),
-    Container(),
+    GuideNotificationsPage(),
     Container(),
   ];
   int cIndex = 0;
@@ -39,8 +40,8 @@ class _GuideDashbboardPageState extends State<GuideDashbboardPage> {
         unselectedFontSize: 10.0,
         elevation: 0.0,
         items: [
-          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
-          BottomNavigationBarItem(label: "Bar", icon: Icon(Icons.notifications)),
+          BottomNavigationBarItem(label: "Trips", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "Bookings", icon: Icon(Icons.notifications)),
           // BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
           BottomNavigationBarItem(label: "Profile", icon: Icon(Icons.person)),
         ],
--- lib/pages/guide/navpages/home.dart
+++ lib/pages/guide/navpages/home.dart
@@ -13,13 +13,10 @@ class _GuideHomePageState extends State<GuideHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        title: AppText('All Packages'),
-      ),
+      appBar: AppBar(elevation: 0.0, title: AppText('All Packages')),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
               ListView.separated(
