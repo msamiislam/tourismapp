691909485bec24efd8e9f6469d7c1af68b058db9
sami-islam99 <sami.islam786@gmail.com>
2021-12-19 17:23:08 +0500

    update itinerary page

--- lib/pages/guide/add_trip/trip_itinerary.dart
+++ lib/pages/guide/add_trip/trip_itinerary.dart
@@ -1,68 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
-import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:tourismapp/widgets/simple_txt.dart';
 
-import 'add_trip_controller.dart';
-
 class TripItineraryPage extends StatelessWidget {
-  final AddTripController _tripController = Get.find();
-  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+  final int days;
 
-  TripItineraryPage({Key? key}) : super(key: key);
+  TripItineraryPage({Key? key, required this.days}) : super(key: key);
+
+  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+  List<Map> tripItinerary = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Center(child: AppText('Step 2 of 2', size: 16.0, weight: FontWeight.w600)),
+          SizedBox(width: 10.0),
+        ],
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
         child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               FormBuilder(
                 key: _fbKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AppText(
-                      'Day ${_tripController.currentPage + 1}',
-                      size: 16.0,
-                      weight: FontWeight.w600,
-                    ),
-                    SizedBox(height: 15.0),
-                    GetBuilder<AddTripController>(
-                      didUpdateWidget: (oldWidget, state) => state.controller!.currentDay == _tripController.currentDay,
-                      builder: (context) => ListView.builder(
-                          primary: false,
-                          shrinkWrap: true,
-                          itemCount: _tripController.currentDay.activitiesCount,
-                          itemBuilder: (context, index) {
-                            return Stack(
-                              alignment: Alignment.topRight,
-                              children: [
-                                ActivityFields(
-                                  time: _tripController.currentDay.activities[index].time,
-                                  desc: _tripController.currentDay.activities[index].description,
-                                  onTimeChanged: _tripController.currentDay.activities[index].updateTime,
-                                  onDescriptionChanged: _tripController.currentDay.activities[index].updateDescription,
-                                ),
-                                if (index != 0 && index == _tripController.currentDay.activitiesCount - 1)
-                                  IconButton(
-                                    onPressed: _tripController.removeActivityFromCurrentDay,
-                                    icon: Icon(Icons.delete, color: Theme.of(context).colorScheme.primary),
-                                  ),
-                              ],
-                            );
-                          },
-                        )
-                    ),
-                    AddButton(
-                      onTap: _tripController.addActivityToCurrentDay,
-                      title: 'Add Activity',
-                    ),
-                  ],
+                child: ListView.separated(
+                  primary: false,
+                  shrinkWrap: true,
+                  itemCount: days,
+                  itemBuilder: (context, index) => DayActivities(
+                    day: index + 1,
+                    onActivityChanged: (int activities) {
+                      tripItinerary.add({'day': index + 1, 'activities': activities});
+                    },
+                  ),
+                  separatorBuilder: (BuildContext context, int index) => SizedBox(height: 15.0),
                 ),
               ),
             ],
@@ -70,60 +47,139 @@ class TripItineraryPage extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Row(
-          children: [
-            if (!_tripController.isFirstPage) ...[
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: _tripController.goToPreviousPage,
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.secondary),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: AppText('Previous'),
-                  ),
-                ),
-              ),
-              SizedBox(width: 10.0),
-            ],
-            Expanded(
-              child: ElevatedButton(
-                onPressed: _tripController.goToNextPage,
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(_tripController.isLastPage
-                      ? Theme.of(context).colorScheme.primary
-                      : Theme.of(context).colorScheme.secondary),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: AppText(_tripController.isLastPage ? 'Save' : "Next"),
-                ),
-              ),
-            ),
-          ],
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: ElevatedButton(
+          onPressed: () {
+            _fbKey.currentState!.save();
+
+            if (_fbKey.currentState!.validate()) {
+              for (var map in tripItinerary) {
+                int theDay = map['day'];
+                for (int i = 0; i < map['activities']; i++) {
+                  int theActivity = i + 1;
+                  print(
+                      'Day $theDay - ${_fbKey.currentState!.value['time$theActivity-$theDay']} - ${_fbKey.currentState!.value['desc$theActivity-$theDay']}');
+                }
+              }
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text('Add Trip'),
+          ),
         ),
       ),
     );
   }
 }
 
-class ActivityFields extends StatefulWidget {
-  final DateTime? time;
-  final String? desc;
-  final void Function(DateTime? time) onTimeChanged;
-  final void Function(String? time) onDescriptionChanged;
+class DayActivities extends StatefulWidget {
+  final int day;
+  final void Function(int activities) onActivityChanged;
 
-  const ActivityFields(
-      {Key? key, this.time, this.desc, required this.onTimeChanged, required this.onDescriptionChanged})
-      : super(key: key);
+  const DayActivities({Key? key, required this.day, required this.onActivityChanged}) : super(key: key);
 
   @override
-  State<ActivityFields> createState() => _ActivityFieldsState();
+  State<DayActivities> createState() => _DayActivitiesState();
 }
 
-class _ActivityFieldsState extends State<ActivityFields> {
+class _DayActivitiesState extends State<DayActivities> {
+  int timing = 1;
+  int description = 1;
+  List<Widget> activities = [];
+  bool showActivities = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.day == 1) {
+      showActivities = true;
+    }
+    activities.add(
+      Activity(
+        timeName: 'time${timing++}-${widget.day}',
+        descName: 'desc${description++}-${widget.day}',
+      ),
+    );
+    widget.onActivityChanged.call(activities.length);
+  }
+
+  addActivity() {
+    setState(() {
+      activities.add(
+        Activity(
+          timeName: 'time${timing++}-${widget.day}',
+          descName: 'desc${description++}-${widget.day}',
+        ),
+      );
+      widget.onActivityChanged.call(activities.length);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        InkWell(
+          hoverColor: Colors.transparent,
+          focusColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          child: SizedBox(
+            height: 48.0,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AppText('Day ${widget.day}', size: 16.0, weight: FontWeight.w600),
+                Spacer(),
+                !showActivities ? Icon(Icons.keyboard_arrow_down) : Icon(Icons.keyboard_arrow_up),
+              ],
+            ),
+          ),
+          onTap: () {
+            if (showActivities) {
+              setState(() {
+                showActivities = false;
+              });
+            } else {
+              setState(() {
+                showActivities = true;
+              });
+            }
+          },
+        ),
+        Visibility(
+          maintainState: true,
+          visible: showActivities,
+          child: Column(
+            children: [
+              ListView.separated(
+                primary: false,
+                shrinkWrap: true,
+                itemCount: activities.length,
+                itemBuilder: (context, index) => activities[index],
+                separatorBuilder: (BuildContext context, int index) => SizedBox(height: 12.5),
+              ),
+              SizedBox(height: 10.0),
+              AddButton(
+                onTap: () => addActivity(),
+                title: 'Add Activity',
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class Activity extends StatelessWidget {
+  final String timeName;
+  final String descName;
+
+  const Activity({Key? key, required this.timeName, required this.descName}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -132,40 +188,30 @@ class _ActivityFieldsState extends State<ActivityFields> {
         SizedBox(
           width: 90.0,
           child: FormBuilderDateTimePicker(
-            name: "time",
-            initialValue: widget.time,
+            name: timeName,
             inputType: InputType.time,
             decoration: InputDecoration(
               hintText: '9:00 am',
               border: OutlineInputBorder(),
             ),
-            onChanged: (time) {
-              print(time);
-              widget.onTimeChanged(time);
-              },
             format: DateFormat.jm(),
             alwaysUse24HourFormat: false,
-            validator: FormBuilderValidators.compose([
-              FormBuilderValidators.required(context),
-            ]),
+            validator: FormBuilderValidators.compose([FormBuilderValidators.required(context)]),
           ),
         ),
         SizedBox(height: 10.0),
         FormBuilderTextField(
-          name: "description",
-          initialValue: widget.desc,
+          name: descName,
           minLines: 1,
           maxLines: 3,
           decoration: InputDecoration(
             hintText: 'Description',
             border: OutlineInputBorder(),
           ),
-          onChanged: widget.onDescriptionChanged,
-          validator: FormBuilderValidators.compose([
-            FormBuilderValidators.required(context),
-          ]),
+          validator: FormBuilderValidators.compose(
+            [FormBuilderValidators.required(context)],
+          ),
         ),
-        SizedBox(height: 10.0),
       ],
     );
   }
@@ -179,29 +225,30 @@ class AddButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topRight,
-      child: InkWell(
-        onTap: onTap,
-        child: Container(
-          margin: const EdgeInsets.only(top: 5.0),
-          padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
-          width: 150.0,
-          decoration: BoxDecoration(
-            border: Border.all(color: Theme.of(context).colorScheme.primary),
-            borderRadius: BorderRadius.all(Radius.circular(25.0)),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(Icons.add, color: Theme.of(context).colorScheme.primary),
-              SizedBox(width: 5.0),
-              AppText(title, size: 16.0, color: Theme.of(context).colorScheme.primary),
-            ],
+    return Row(
+      children: [
+        Spacer(),
+        InkWell(
+          onTap: onTap,
+          child: Container(
+            margin: const EdgeInsets.only(top: 5.0),
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).colorScheme.primary),
+              borderRadius: BorderRadius.all(Radius.circular(25.0)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.add, color: Theme.of(context).colorScheme.primary),
+                SizedBox(width: 5.0),
+                AppText(title, size: 16.0, color: Theme.of(context).colorScheme.primary),
+              ],
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
